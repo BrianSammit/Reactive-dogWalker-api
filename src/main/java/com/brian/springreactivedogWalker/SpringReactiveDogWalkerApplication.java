82a9599fae7cecac58757b79ceb71c73a1c878a7
BrianSammit <briansammit.cruz@gmail.com>
2023-03-23 13:00:51 -0500

    first commit

---
+++ src/main/java/com/brian/springreactivedogWalker/SpringReactiveDogWalkerApplication.java
@@ -0,0 +1,13 @@
+package com.brian.springreactivedogWalker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactiveDogWalkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactiveDogWalkerApplication.class, args);
+	}
+
+}
